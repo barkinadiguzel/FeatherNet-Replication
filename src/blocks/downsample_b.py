85398d24a91b52198ffc58062e69e52b6207e37b
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-04-27 12:37:47 +0300

    Update downsample_b.py

--- src/blocks/downsample_b.py
+++ src/blocks/downsample_b.py
@@ -6,18 +6,22 @@ class DownsampleB(nn.Module):
         super().__init__()
 
         self.branch1 = nn.Sequential(
-            nn.Conv2d(inp, oup, 3, stride=2, padding=1,
-                      groups=inp, bias=False),
-            nn.BatchNorm2d(oup)
+            nn.Conv2d(inp, inp, 3, stride=2, padding=1, groups=inp, bias=False),
+            nn.BatchNorm2d(inp)
         )
-
         self.branch2 = nn.Sequential(
-            nn.AvgPool2d(2, stride=2),
+            nn.AvgPool2d(kernel_size=2, stride=2),
             nn.Conv2d(inp, oup, 1, bias=False),
             nn.BatchNorm2d(oup)
         )
 
-        self.act = nn.ReLU(inplace=True)
+        self.bn = nn.BatchNorm2d(oup)
+        self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
-        return self.act(self.branch1(x) + self.branch2(x))
+        x1 = self.branch1(x)
+        x2 = self.branch2(x)
+
+        x = x1 + x2
+
+        return self.relu(self.bn(x))
